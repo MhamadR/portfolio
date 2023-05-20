b7c3837c2642cab4b1ca5ca4aadafc0915926d5f
MhamadR <mhamadraslan@gmail.com>
2023-05-20 14:09:55 +0300

    Add Project component and Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Scene from "./components/Scene";
 import Room from "./components/Room";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <Scene>
       <Room />
+      <Projects />
     </Scene>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import "../assets/projects.css";
+
+function Projects() {
+  return (
+    <>
+      <div className="try">
+        <p>Helloooo</p>
+      </div>
+    </>
+  );
+}
+
+export default Projects;
